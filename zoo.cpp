c87d39fa87e322691f0478bacce9a25568905b48
ToyodaMayu <mny.8-hunny1@docomo.ne.jp>
2020-03-11 11:37:27 +0900

    ピラミッド作成

--- zoo.cpp
+++ zoo.cpp
@@ -2,15 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>     
+#include <ctime>        
+#include <cstdlib>
 
 int bord[10][15] =
 { {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
-  {0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
-  {0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
-  {0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
-  {0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
-  {0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+  {0,0,0,0,1,1,1,1,1,1,1,0,0,0,0},
+  {0,0,0,1,1,1,1,1,1,1,1,0,0,0,0},
+  {0,0,0,1,1,1,1,1,1,1,1,1,0,0,0},
+  {0,0,1,1,1,1,1,1,1,1,1,1,0,0,0},
+  {0,0,1,1,1,1,1,1,1,1,1,1,1,0,0},
+  {0,1,1,1,1,1,1,1,1,1,1,1,1,0,0},
   {0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
   {2,2,2,2,2,2,2,2,2,2,2,2,2,2,0} };
@@ -19,14 +22,16 @@ int card[36] = { 1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5
 
 void hand(void);
 int random(int min, int max);
+void even(void);
+void odd(void);
 
 typedef struct player {
 	char name[30];
 	int attack;
 	int hand[14];
 }PLAYER;
-PLAYER you = { "あなた" };
-PLAYER com = { "コンピュータ" };
+PLAYER you;
+PLAYER com;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -40,7 +45,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return -1;
 	}
 
+	LoadGraphScreen(0, 0, "back.png", TRUE);
+	ScreenFlip();
+
 	hand();
+
+	even();
+	odd();
 
 	WaitKey();
 	DxLib_End();
@@ -71,6 +82,7 @@ void hand(void)
 	}
 
 	int y = 0;
+	int YOU = 0;
 	for (int i = 0; i < 10; i++)
 	{
 		int x = 30;
@@ -78,27 +90,32 @@ void hand(void)
 		{
 			if (bord[i][j] == 2)
 			{
-				switch (you.hand[j])
+				switch (you.hand[YOU])
 				{
 				case 1:
 					LoadGraphScreen(x, y, "g.png", TRUE);
 					ScreenFlip();
+					YOU++;
 					break;
 				case 2:
 					LoadGraphScreen(x, y, "b.png", TRUE);
 					ScreenFlip();
+					YOU++;
 					break;
 				case 3:
 					LoadGraphScreen(x, y, "y.png", TRUE);
 					ScreenFlip();
+					YOU++;
 					break;
 				case 4:
 					LoadGraphScreen(x, y, "r.png", TRUE);
 					ScreenFlip();
+					YOU++;
 					break;
 				case 5:
 					LoadGraphScreen(x, y, "p.png", TRUE);
 					ScreenFlip();
+					YOU++;
 					break;
 				}
 			}
@@ -111,4 +128,42 @@ void hand(void)
 int random(int min, int max)
 {
 	return min + (int)(rand() * (max - min + 1.0) / (1.0 + RAND_MAX));
+}
+
+void even(void)
+{
+	int y = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		int x = 30;
+		for (int j = 0; j < 15; j++)
+		{
+			if (bord[i][j] == 1 && i % 2 == 0)
+			{
+				LoadGraphScreen(x, y, "g.png", TRUE);
+				ScreenFlip();
+			}
+			x += 60;
+		}
+		y += 90;
+	}
+}
+
+void odd(void)
+{
+	int y = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		int x = 0;
+		for (int j = 0; j < 15; j++)
+		{
+			if (bord[i][j] == 1 && i % 2 == 1)
+			{
+				LoadGraphScreen(x, y, "g.png", TRUE);
+				ScreenFlip();
+			}
+			x += 60;
+		}
+		y += 90;
+	}
 }
